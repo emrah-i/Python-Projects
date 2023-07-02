b9b8e726b45d271ba5bc4253d50050a48b6531d1
emrah-i <ibraem1026@gmail.com>
2023-07-02 19:49:40 -0400

    Finish pomodoro technique

--- intermediate/pomodoro/main.py
+++ intermediate/pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 
 bgc = '#FEF5E7'
 check = '✔'
@@ -8,29 +9,56 @@ window.config(bg=f'{bgc}', pady=50, padx=50)
 window.title('Pomodoro Technique')
 window.minsize(300,300)
 
-minutes = 0
-seconds = 3
+minutes = 24
+seconds = 60
 score = 0
+type = 'timer'
+stop = False
+
+def begin():
+    global stop
+    
+    if minutes == 24 and seconds == 60 and score == 0:
+        status.config(text='Work', fg='green')
+
+    stop = False
+    if type == 'timer':
+        timer()
+    elif type == 'break':
+        break_timer()
+
+    return
 
 def timer():
     global minutes
     global seconds
     global score
+    global stop
 
-    if minutes >= 0 and seconds > 0 and score < 5:
-        if seconds > 0:
-            seconds -= 1
-        elif seconds == 0 and minutes > 0:
-            minutes -= 1
-        canvas.itemconfig(text_item, text=f'{minutes}:{seconds}')
-        window.after(1000, timer)
-    elif minutes == 0 and seconds == 0 and score < 5:
-        minutes = 4
-        seconds = 60
-        score += 1
-        canvas.itemconfig(text_item, text=f'{minutes}:{seconds}')
-        checks.config(text=f'{check * score}')
-        break_timer()
+    if stop == False:
+        if minutes >= 0 and seconds > 0 and score < 5:
+            if seconds > 0:
+                seconds -= 1
+            elif seconds == 0 and minutes > 0:
+                minutes -= 1
+            canvas.itemconfig(text_item, text=f"{minutes:02d}:{seconds:02d}")
+            window.after(1000, timer)
+        elif minutes == 0 and seconds == 0 and score < 4:
+            minutes = 4
+            seconds = 60
+            score += 1
+            status.config(text='Break', fg='salmon')
+            canvas.itemconfig(text_item, text=f"{minutes:02d}:{seconds:02d}")
+            checks.config(text=f'{check * score}')
+            break_timer()
+        elif minutes == 0 and seconds == 0 and score == 4:
+            minutes = 19
+            seconds = 60
+            score += 1
+            status.config(text='Break', fg='salmon')
+            canvas.itemconfig(text_item, text=f"{minutes:02d}:{seconds:02d}")
+            checks.config(text=f'{check * score}')
+            break_timer()
         
     return
 
@@ -38,23 +66,56 @@ def break_timer():
     global minutes
     global seconds
 
-    if minutes >= 0 and seconds > 0:
-        if seconds > 0:
-            seconds -= 1
-        elif seconds == 0 and minutes > 0:
-            minutes -= 1
-        canvas.itemconfig(text_item, text=f'{minutes}:{seconds}')
-        window.after(1000, break_timer)
-    elif minutes == 0 and seconds == 0:
-        minutes = 24
-        seconds = 60
-        timer()
+    if stop == False:
+        if minutes >= 0 and seconds > 0:
+            if seconds > 0:
+                seconds -= 1
+            elif seconds == 0 and minutes > 0:
+                minutes -= 1
+            canvas.itemconfig(text_item, text=f"{minutes:02d}:{seconds:02d}")
+            window.after(1000, break_timer)
+        elif minutes == 0 and seconds == 0 and score < 5:
+            minutes = 24
+            seconds = 60
+            status.config(text='Work', fg='green')
+            canvas.itemconfig(text_item, text=f"{minutes:02d}:{seconds:02d}")
+            timer()
+        elif minutes == 0 and seconds == 0 and score == 5:
+            status.config(text='Done', fg='black')
+            canvas.itemconfig(text_item, text=f'00:00')
+        
+    return
 
-    break_timer()
+def stop_temp():
+    global stop
+    global type
 
-def reset():
+    if status['text'] == 'Work':
+        type = 'timer'
+    else: 
+        type = 'break'
+
+    stop = True
+    return
+
+def reset_button():
+    global score
+    global minutes
+    global seconds
+    global stop
+    global type
+
+    stop = True
+    type = 'timer'
+    time.sleep(.25)
+    minutes = 24
+    seconds = 60
+    score = 0
+
+    status.config(text='Timer', fg='gray')
+    checks.config(text=f'{check * score}')
     canvas.itemconfig(text_item, text='00:00')
-
+    return
 
 canvas = Canvas(width=200, height=224, bg=bgc, highlightthickness=0)
 tomato_img = PhotoImage(file='intermediate/pomodoro/tomato.png')
@@ -63,16 +124,20 @@ text_item = canvas.create_text(100, 130, text='00:00', font=('Courier', 35, 'bol
 canvas.grid(column=2, row=2)
 
 status = Label()
-status.config(text='Work', font=('Courier', 45, 'normal'), bg=bgc)
+status.config(text='Timer', font=('Courier', 45, 'normal'), bg=bgc, fg='gray')
 status.grid(column=2, row=1)
 
 start = Button()
-start.config(text='Start', highlightbackground=bgc, command=timer)
+start.config(text='Start', highlightbackground=bgc, command=begin)
 start.grid(column=1, row=3)
 
 reset = Button()
-reset.config(text='Reset', highlightbackground=bgc, command=reset)
+reset.config(text='Reset', highlightbackground=bgc, command=reset_button)
 reset.grid(column=3, row=3)
+
+stop_button = Button()
+stop_button.config(text='Stop', highlightbackground=bgc, command=stop_temp)
+stop_button.grid(column=2, row=4)
 
 checks = Label()
 checks.config(text='', font=('Courier', 30, 'normal'), bg=bgc, fg='green')
