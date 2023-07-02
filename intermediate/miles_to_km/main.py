67f72006634f41e77591d6ceb78faa84b43ef4a4
emrah-i <ibraem1026@gmail.com>
2023-07-02 16:24:41 -0400

    Created miles to km converter

--- intermediate/miles_to_km/main.py
+++ intermediate/miles_to_km/main.py
@@ -1,14 +1,33 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Miles to km")
-window['bg'] = 'white'
-window.minsize(400, 400)
+window.minsize(300, 150)
+window.config(bg='aliceblue', padx=30, pady=30)
 
-my_label = tkinter.Label(text='Enter miles: ', font=('Arial', 23, 'bold'))
-my_label['bg'] = 'white'
-my_label.pack(expand=1)
+def convert_miles():
+    numb = int(miles.get())
+    numb_convert = round((numb * 1.6093), 2)
+    km.config(text=f'{numb_convert}')
+    return
 
+miles = Entry(width=8, highlightbackground='aliceblue')
+miles.grid(column=2, row=1)
+
+m_label = Label(text='miles', font=('Arial', 15, 'normal'), bg='aliceblue')
+m_label.grid(column=3, row=1)
+
+km = Label(text='0', font=('Arial', 15, 'normal'), bg='aliceblue')
+km.grid(column=2, row=2)
+
+km_label = Label(text='kilometers', font=('Arial', 15, 'normal'), bg='aliceblue')
+km_label.grid(column=3, row=2)
+
+m_to_km_label = Label(text='is equal to:', font=('Arial', 15, 'normal'), bg='aliceblue')
+m_to_km_label.grid(column=1, row=2)
+
+convert = Button(text='calculate', command=convert_miles, highlightbackground='aliceblue')
+convert.grid(column=2, row=3)
 
 
 
