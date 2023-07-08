03cbffbced118c3b28053bb963987e8be12413a2
emrah-i <ibraem1026@gmail.com>
2023-07-08 01:56:55 -0400

    Start spotify project

--- intermediate+=/playlist_maker/main.py
+++ intermediate+=/playlist_maker/main.py
@@ -1,4 +1,6 @@
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 
 date = input('What week would you like to get a playlist for(YYYY-MM-DD): ')
@@ -18,5 +20,21 @@ artists = soup.select('div[class="o-chart-results-list-row-container"] ul li[cla
 
 artists = [name.text for name in artists if name.get("class")[1] == 'a-no-trucate']
 
-for i in range(len(artists)):
-   print(i, artists[i].strip(), "-", song[i].text.strip())
+songs = [f"{artists[i].strip()}-{song[i].text.strip()}" for i in range(len(artists))]
+
+client_id = '6e8733d5c93346929371e20f250ba4c1'
+client_secret = '85382ad5178145afae9198ba9e8b16e3'
+
+scope = "user-library-read"
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(show_dialog=True, username='emrakh26', cache_path='intermediate+=/playlist_maker/info.text', client_id=client_id, client_secret=client_secret, redirect_uri="http://example.com", scope="playlist-modify-private"))
+user = sp.current_user()
+sp.user_playlist_create(name="12342", user=user['id'])
+
+'''song_ids = []
+for song in songs:
+    item = sp.search(song, limit=1)
+    song_ids.append(item['tracks']['items'][0]['id'])
+    
+sp.playlist_add_items(items=song_ids)'''
