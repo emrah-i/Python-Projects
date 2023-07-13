0150edfe29096251891c5d4ef8a2e46e042ddbad
emrah-i <ibraem1026@gmail.com>
2023-07-12 21:28:43 -0400

    Quick updates to bookshelf

--- web/bookshelf/main.py
+++ web/bookshelf/main.py
@@ -1,11 +1,30 @@
 from flask import render_template, redirect, request
 from models import app, Book
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    return render_template('index.html')
+
+@app.route('/read')
+def read():
+    return render_template('index.html')
+
+@app.route('/unread')
+def unread():
+    return render_template('index.html')
+
+@app.route('/read_list')
+def read_list():
+    return render_template('index.html')
+
+@app.route('/recommend')
+def recommend():
     return render_template('index.html')
