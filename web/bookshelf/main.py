d01390500ff3dcb597952bc2ed53bd40603ced1d
emrah-i <ibraem1026@gmail.com>
2023-07-13 01:55:10 -0400

    Added models, forms, templates, and images to bookshelf

--- web/bookshelf/main.py
+++ web/bookshelf/main.py
@@ -1,9 +1,13 @@
-from flask import render_template, redirect, request
-from models import app, Book
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from flask import render_template, redirect, request, url_for
+from models import app, db, Books, BookForm, Delete
+from flask_bootstrap import Bootstrap5
+import requests
+
+app.config['SECRET_KEY'] = 'ilovecats'
+bootstrap = Bootstrap5(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def main():
@@ -11,19 +15,51 @@ def main():
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
-    return render_template('index.html')
+    results = db.session.query(Books.title, Books.author).all()
+    books = [f"{row.title} --- {row.author}" for row in results]
+    books.insert(0, "")
+    add = BookForm()
+    delete = Delete()
+    delete.book.choices = books
+    if request.method == 'POST':
+        if add.validate_on_submit() or delete.validate_on_submit():
+            type = request.form.get('method')
+            if type == 'add':
+                    title = add.title.data.strip()
+                    author = add.author.data.strip()
+                    read = add.read.data
+                    response = requests.get(f'https://api.bookcover.longitood.com/bookcover?book_title={title}&author_name={author}')
+                    data = response.json()
+                    img = data['url']
+                    new_book = Books(title=title, author=author, img=img, read=read)
+                    db.session.add(new_book)
+                    db.session.commit()
+                    return redirect('/edit')
+            elif type == 'delete':
+                    title = delete.book.data.split(' --- ')[0]
+                    entry = db.session.query(Books).filter(Books.title == title).first()
+                    db.session.delete(entry)
+                    db.session.commit()
+                    return redirect('/edit')
+        else:
+            message = "<p style='color:red'>*ERROR*</p>"
+            return render_template('edit.html', add=add, delete=delete, message=message)
+    else:
+        return render_template('edit.html', add=add, delete=delete)
 
 @app.route('/read')
 def read():
-    return render_template('index.html')
+    results = db.session.query(Books).filter(Books.read == True).all()
+    return render_template('books.html', books=results)
 
 @app.route('/unread')
 def unread():
-    return render_template('index.html')
+    results = db.session.query(Books).filter(Books.read == False).all()
+    return render_template('books.html', books=results)
 
 @app.route('/read_list')
 def read_list():
-    return render_template('index.html')
+    return render_template('books.html')
 
 @app.route('/recommend')
 def recommend():
--- web/bookshelf/models.py
+++ web/bookshelf/models.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Boolean
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, ValidationError, BooleanField, PasswordField, SelectField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db.init_app(app)
 
-class Book(db.Model):
+def must_equal_validator(form, field):
+    if field.data != "ilovecats":
+        raise ValidationError("Key is wrong. You cannot add books to the database.")
+
+class Books(db.Model):
     id = Column(Integer, primary_key=True)
     title = Column(String(150), unique=True, nullable=False)
-    author = Column(String(100), unique=True)
+    author = Column(String(100))
     img = Column(String(250))
     read = Column(Boolean, default=False)
+
+class BookForm(FlaskForm):
+    key = PasswordField('Key:', validators=[DataRequired(), must_equal_validator])
+    title = StringField('Title:', validators=[DataRequired(), Length(min=2, max=150)])
+    author = StringField('Author:', validators=[DataRequired(), Length(min=2, max=150)])
+    read = BooleanField('Read')
+    submit = SubmitField()
+
+class Delete(FlaskForm):
+    key = PasswordField('Key:', validators=[DataRequired(), must_equal_validator])
+    book = SelectField('Book:', validators=[DataRequired()])
+    delete = SubmitField(render_kw={'value': 'Delete', 'style': 'background-color: #E74C3C;border-color: #E74C3C'})
