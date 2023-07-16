5fd577b843d8fc143b5541042109c0dfe355423e
emrah-i <ibraem1026@gmail.com>
2023-07-15 22:48:55 -0400

    Updating homepage button actions

--- web/top_movies/static/script.js
+++ web/top_movies/static/script.js
@@ -2,11 +2,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     if (document.querySelector('#update_button')) {
-        document.querySelector('#update_button').addEventListener('click', (event) => {
-            id = event.target.dataset.id;
-            event.target.style.display = 'none';
-            form = document.querySelector('#update_form');
-            form.style.display = 'block'
+        document.querySelectorAll('#update_button').forEach((element) => {
+            element.addEventListener('click', (event) => {
+                id = event.target.dataset.id;
+                console.log(id)
+                document.querySelector('#delete_button[data-id="' + id + '"]').style.display = 'none';
+                event.target.style.display = 'none';
+                form = document.querySelector('#update_form' + id);
+                form.style.display = 'block'
+            })
+        })
+    }
+
+    if (document.querySelector('#delete_button')) {
+        document.querySelectorAll('#delete_button').forEach((element) => {
+            element.addEventListener('click', (event) => {
+                if (confirm("Are you sure you would like to delete this movie?")) {
+
+                }
+            })
         })
     }
 })
