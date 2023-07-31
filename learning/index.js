a19785d855d310876ab9a923dd7508cd39817cee
emrah-i <ibraem1026@gmail.com>
2023-07-31 04:49:01 -0400

    Started JS to do list

--- learning/index.js
+++ learning/index.js
@@ -1,13 +1,32 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+function logger(req, res, next) {
+    console.log(`Request Method: ${req.method}`);
+    console.log(`Request URL: ${req.url}`);
+    next();
+}
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'))
+app.use(morgan("tiny"))
+app.use(logger)
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-    res.send('<h1>Ahh Hello, World!</h1>')
+    res.render("index.ejs", {
+        day: 'weekday',
+        advice: 'hi'
+    })
 })
 
 app.listen(port, () => {
-    console.log('Server running on port 3000.');
+    console.log(`Server running on port ${port}.`);
 })
 
