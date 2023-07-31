a1a60074cf7b5b9501056ccc8c8059ddac532079
emrah-i <ibraem1026@gmail.com>
2023-07-31 00:11:19 -0400

    Express.js practice

--- learning/index.js
+++ learning/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
-import inquirer from 'inquirer'
-import fs from 'fs'
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Ahh Hello, World!</h1>')
+})
+
+app.listen(port, () => {
+    console.log('Server running on port 3000.');
+})
 
