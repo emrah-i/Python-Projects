c85cb1972e6e4e066662c4ce2faee3ad9211dc56
emrah-i <ibraem1026@gmail.com>
2023-08-04 23:57:49 -0400

    Modified homepage form, add to clipboard buttons, and responsiveness.

--- secrets/public/html.js
+++ secrets/public/html.js
@@ -2,7 +2,9 @@
 const a_ch = document.querySelector('#about_chevron');
 const p_ch = document.querySelector('#projects_chevron');
 const resume_button = document.querySelector('button.resume');
-const chars = document.querySelector('#chars')
+const chars = document.querySelector('#chars');
+const email = document.querySelector('#personal_email');
+const phone = document.querySelector('#personal_phone');
 const about_element = document.querySelector('#about_section');
 const quote = document.querySelector('.quote')
 const lightColor = '#FBF7F2';
@@ -28,7 +30,7 @@ if (a_ch) {
 
 if (p_ch) {
     p_ch.addEventListener('click', ()=>{
-        document.querySelector('#project1').scrollIntoView({ block: 'center' });
+        document.querySelector('#project1').scrollIntoView({block: 'center'});
     })
 }
 
@@ -47,8 +49,38 @@ if (chars) {
     })
 }
 
+if (email) {
+    const email_info = document.querySelector('#personal_email .tooltiptext')
+    document.querySelector('.email-btn').addEventListener('click', ()=> {
+        email.scrollIntoView({block: 'center'});
+    });
+
+    document.querySelector('#personal_email > span').addEventListener('click', ()=> {
+        navigator.clipboard.writeText('ibraem1026@gmail.com');
+        email_info.innerHTML = '✔'
+        setTimeout(()=>{
+            email_info.innerHTML = 'Click to copy!'
+        }, 1000)
+    });
+};
+
+if (phone) {
+    const phone_info =  document.querySelector('#personal_phone .tooltiptext')
+    document.querySelector('.phone-btn').addEventListener('click', ()=> {
+        phone.scrollIntoView({block: 'center'});
+    });
+
+    document.querySelector('#personal_phone > span').addEventListener('click', ()=> {
+        navigator.clipboard.writeText('240-520-8934');
+        phone_info.innerHTML = '✔'
+        setTimeout(()=>{
+            phone_info.innerHTML = 'Click to copy!'
+        }, 1000)
+    });
+};
+
 eye.addEventListener('click', (event)=> {
-    let color = ''
+    let color = '';
 
     if (event.target.id === 'eye') {
         color = event.target.dataset.color
@@ -56,7 +88,7 @@ eye.addEventListener('click', (event)=> {
     else {
         color = event.target.closest('#eye').dataset.color
         console.log(color)
-    }
+    };
 
     if (color === 'light') {
         window.localStorage.setItem('color', 'dark');
@@ -67,5 +99,5 @@ eye.addEventListener('click', (event)=> {
         window.localStorage.setItem('color', 'light');
         document.documentElement.style.setProperty('--main-color', lightColor);
         eye.dataset.color = 'light';
-    }
+    };
 })
