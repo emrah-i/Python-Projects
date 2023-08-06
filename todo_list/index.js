f61ba0d5723644a3e8becc327b006a5a19e1aa71
emrah-i <ibraem1026@gmail.com>
2023-08-06 01:39:00 -0400

    MongoDB addition to todo list

--- todo_list/index.js
+++ todo_list/index.js
@@ -1,50 +1,62 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import { MongoClient } from 'mongodb'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-function logger(req, res, next) {
-    console.log(`Request Method: ${req.method}`);
-    console.log(`Request URL: ${req.url}`);
-    next();
-}
+const uri = "mongodb://127.0.0.1:27017";
 
-var all_items = []
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+    await client.connect();
+    const database = client.db('todoDB');
+    const all_items = database.collection('items');
+    return all_items
+}
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
 app.use(morgan("tiny"))
-app.use(logger)
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.render("index.ejs", {
-        all_items: all_items
-    })
-})
+app.get('/', async (req, res) => {
+    const all_items = await run();
+    const items = await all_items.find({}).toArray();
+    await client.close();
 
-app.post('/new', (req, res) => {
+    res.render('index.ejs', { all_items: items });
+});
+
+app.post('/new', async (req, res) => {
     var item = {
         item: req.body['item'],
         date: req.body['date'],
         time: req.body['time']
     }
-    all_items.push(item)
+    
+    const all_items = await run();
+    await all_items.insertOne(item);
+    await client.close();
     res.redirect('/')
 })
 
-app.post('/remove', (req, res) => {
+app.post('/remove', async (req, res) => {
     var item = {
         item: req.body['item'],
         date: req.body['date'],
         time: req.body['time']
     }
-    var index = all_items.indexOf(item)
-    all_items.splice(index, 1)
+    const all_items = await run();
+    await all_items.deleteOne(item);
+    await client.close();
     res.redirect('/')
 })
 
